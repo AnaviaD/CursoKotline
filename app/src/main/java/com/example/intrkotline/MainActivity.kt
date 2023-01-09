6385900da29d74365f1b8bb30fc1772102c43aca
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-09 10:26:39 -0600

    Add class 1 variable

--- app/src/main/java/com/example/intrkotline/MainActivity.kt
+++ app/src/main/java/com/example/intrkotline/MainActivity.kt
@@ -4,8 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    companion object{
+        const val moneda: String = "EUR"
+    }
+    var saldo: Float = 300.54f
+    var sueldo = 764.82
+    var entero: Int = 62
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fecha: String = "05/06/1990"
+        var nombre: String = "jota"
+        var vip: Boolean = false
+        var inicial: Char = 'J'
+
+        var saludo: String = "Hola " + nombre + " " + saldo.toString()
+
+
+        println(fecha)
+        println("Hola " + nombre)
+        println(saludo)
+
     }
 }
