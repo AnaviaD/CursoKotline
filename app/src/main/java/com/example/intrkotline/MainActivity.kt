80a030f5df262d7c714f6a18c761578921defa70
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-09 11:08:08 -0600

    Add first conditionals

--- app/src/main/java/com/example/intrkotline/MainActivity.kt
+++ app/src/main/java/com/example/intrkotline/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         const val moneda: String = "EUR"
     }
     var saldo: Float = 300.54f
-    var sueldo = 764.82
+    var sueldo: Float = 764.82f
     var entero: Int = 62
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +20,48 @@ class MainActivity : AppCompatActivity() {
         var inicial: Char = 'J'
 
         var saludo: String = "Hola " + nombre + " " + saldo.toString()
+        if(vip == true) saludo += "Te queremos mucho"
+        else saludo += "Quieres ser vip prro?"
 
 
         println(fecha)
         println("Hola " + nombre)
         println(saludo)
+
+        var a:Int = 5 + 5 //10
+        val b:Int = 10 - 2
+        val c:Int = 3 * 4
+        val d:Int = 10 / 5
+        val e:Int = 10 % 3  //1, no se puede obtener el resto despues del punto
+        val f:Int = 10 / 6  //1, division infinita, solo se mantiene la parte entera
+
+        var aPreIncremento: Int = 5
+        var bPreDecremento: Int = 5
+        var cPostIncremento: Int = 5
+        var dPostDecremento: Int = 5
+
+        //Influye en que posicion esta el  ++ o --
+        println(aPreIncremento)
+        println(++aPreIncremento)
+        println(aPreIncremento)
+
+        println(bPreDecremento)
+        println(--bPreDecremento)
+        println(bPreDecremento)
+
+        println(cPostIncremento)
+        println(cPostIncremento--)
+        println(cPostIncremento)
+
+        println(dPostDecremento)
+        println(dPostDecremento++)
+        println(dPostDecremento)
+
+        saldo += sueldo
+
+        a==b
+
+
 
     }
 }
