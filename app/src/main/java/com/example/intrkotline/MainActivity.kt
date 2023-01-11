5268b543e264605c65edec1b409b3af1882cb849
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-11 17:17:17 -0600

    Null safety

--- app/src/main/java/com/example/intrkotline/MainActivity.kt
+++ app/src/main/java/com/example/intrkotline/MainActivity.kt
@@ -126,9 +126,23 @@ class MainActivity : AppCompatActivity() {
         inversiones.put("Coca-Cola", 50f)
         println(inversiones)
 
-        
+        var empresa: String = "jotajotavm"
 
+        mostrar_saldo()
+        var cantidad_a_invertir = 90f
+        var index001 = 0
 
+        while (saldo >= cantidad_a_invertir){
+            empresa = bolsa.elementAtOrNull(index)
+            if(empresa != null){
+                saldo -= cantidad_a_invertir
+                println("Se ha invertido $cantidad_a_invertir $moneda en $empresa")
+
+                inversiones.put(empresa, cantidad_a_invertir)
+            }
+            else break
+            index001++
+        }
 
         /*
         var a:Int = 5 + 5 //10
