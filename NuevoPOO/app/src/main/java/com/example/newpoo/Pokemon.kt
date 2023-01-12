baa322d65b526c148408df7d24e820437edd4b7d
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-12 17:13:34 -0600

    add protected private y public
    protected es solo para heredar a las clases hijo

--- NuevoPOO/app/src/main/java/com/example/newpoo/Pokemon.kt
+++ NuevoPOO/app/src/main/java/com/example/newpoo/Pokemon.kt
@@ -1,20 +1,55 @@
 package com.example.newpoo
 
-class Pokemon(  private var name:String = "Pok",
-                private var attackPower: Float = 30f,
-                private var life: Float = 100f)
-{
-    fun Pokemon(n: String, aP: Float){
+import android.widget.Toast
+
+open class Pokemon( protected var name:String = "Pok",
+                    protected var attackPower: Float = 30f,
+                    protected var life: Float = 100f)
+    {
+    fun Pokemon(n: String, aP: Float, l: Float){
         this.name = n
         this.attackPower = aP
         this.life = 100f
+        //this.sayHi()
     }
 
-    fun getName(): String{ return this.name}
-    fun getAttackPower(): Float{ return this.attackPower}
-    fun getLife(): Float{ return this.life}
+    internal fun getName(): String{ return this.name}
+    internal fun getAttackPower(): Float{ return this.attackPower}
+    internal fun getLife(): Float{ return this.life}
 
-    fun setName(nam: String){ this.name = nam}
-    fun setAttakPower(pow: Float){ this.attackPower = pow}
-    fun setLife(l: Float){ this.life = l}
+    internal fun setName(nam: String){ this.name = nam}
+    internal fun setAttakPower(pow: Float){ this.attackPower = pow}
+    internal fun setLife(l: Float){ this.life = l}
+
+    //fun sayHi(){ Toast.makeText(maincontext, "Hola!! Soy $name", Toast.LENGTH_LONG).show()}
+
+    fun cure(){this.life = 100f}
+
+    fun evolve(n: String){
+        this.name = n
+        this.attackPower *= 1.20f
+        //this.sayHi()
+    }
+
+    //fun attack(){ Toast.makeText(maincontext, "Al toque prro", Toast.LENGTH_LONG).show()}
+}
+
+class WaterPokemon(n: String = "WPoke", aP: Float = 30f, l: Float = 100f)
+    : Pokemon(n, aP, l)
+{
+    private var maxResistence: Int = 500
+    private var submergedTime: Int = 0
+
+    //public var life: Float = 100f
+
+    fun WaterPokemon(n: String, aP: Float, mR: Int){
+        this.name = n
+        this.attackPower = aP
+        this.life = 100f
+        this.maxResistence = mR
+        //this.sayHi()
+    }
+
+    fun breathe() { this.submergedTime = 0}
+    fun inmerse() { this.submergedTime++ }
 }
