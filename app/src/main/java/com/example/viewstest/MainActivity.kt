2a8faef8639efe9d3e513276b99f164a3bcb3c5d
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-13 13:40:14 -0600

    Add the first text area
    Text area with Id to edith from code

--- app/src/main/java/com/example/viewstest/MainActivity.kt
+++ app/src/main/java/com/example/viewstest/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.viewstest
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tvEjemplo = findViewById<TextView>(R.id.tvEjemplo)
+        tvEjemplo.text = "Cambiado desde Codigo"
+        tvEjemplo.setTextColor(Color.RED)
+        tvEjemplo.setTypeface(Typeface.MONOSPACE, Typeface.BOLD)
+
     }
 }
