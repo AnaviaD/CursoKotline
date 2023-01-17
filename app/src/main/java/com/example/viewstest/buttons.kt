7bc8c522831158bcf29ed1e76915bf67195e4d13
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-17 17:17:54 -0600

    chipGroup Edit

--- app/src/main/java/com/example/viewstest/buttons.kt
+++ app/src/main/java/com/example/viewstest/buttons.kt
@@ -2,12 +2,36 @@ package com.example.viewstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Toast
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class buttons : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buttons)
 
-        var btBoton = findViewById<Button>(R.id.bt)
+        var btBoton = findViewById<ImageButton>(R.id.imageButton)
+        btBoton.setOnClickListener{
+            Toast.makeText(this, "image Button Pulsado", Toast.LENGTH_SHORT).show()
+        }
+
+        var cgCondiciones = findViewById<ChipGroup>(R.id.cdCondiciones)
+        var chip: Chip
+        for (i in 0..cgCondiciones.childCount){
+            chip = cgCondiciones.getChildAt(i) as Chip
+            chip.textAlignment = View.TEXT_ALIGNMENT_CENTER
+
+            chip.setOnCloseIconClickListener{
+                cgCondiciones.removeView(it)
+            }
+            chip.setOnClickListener{
+                var aux = it as Chip
+                Toast.makeText(this, "${aux.text} pulsado", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
