ce4731574395ff209f6c723f1ec2782ddf03bf6f
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-01-31 13:21:14 -0600

    Seems to be, tooble finished ?

--- CursoKotlineIntrod/app/src/main/java/com/example/viewstest/MainActivity.kt
+++ CursoKotlineIntrod/app/src/main/java/com/example/viewstest/MainActivity.kt
@@ -9,44 +9,31 @@ import android.widget.AutoCompleteTextView
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.graphics.blue
 import androidx.core.widget.addTextChangedListener
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var tvEjemplo = findViewById<TextView>(R.id.tvEjemplo)
+        initToolbar()
+    }
 
-        tvEjemplo.apply {
-            text = "Cambiado desde Codigo"
-            setTextColor(Color.RED)
-            setTypeface(Typeface.MONOSPACE, Typeface.BOLD)
-            setOnClickListener{
-                setTextColor(Color.GREEN)
-                text = "No traigo mandados"
-            }
-        }
+    private fun initToolbar(){
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
 
+        drawer = findViewById(R.id.drawer_layout)
+        val toogle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.bar_title, R.string.navigation_drawer_close)
 
-        var etEjemplo = findViewById<EditText>(R.id.etEjemplo)
-        etEjemplo.addTextChangedListener {
-            var inicio:Int
-            var fin:Int
-            inicio = etEjemplo.selectionStart
-            fin = etEjemplo.selectionEnd
-            println("La variable inicio: ${inicio} ")
-            println("La variable fin: ${fin}")
-            println("Valor: ${etEjemplo.text.toString()}")
-            if (etEjemplo.text.length == 0) etEjemplo.setError("Campo vacio")
-        }
+        drawer.addDrawerListener(toogle)
 
-
-        var autoCompleteTextView = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
-        var countries: Array<String> = resources.getStringArray(R.array.countries_array)
-        var adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, countries)
-
-        autoCompleteTextView.setAdapter(adapter)
+        toogle.syncState()
     }
 }
