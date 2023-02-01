8e3fe9c8e9dde1b302e54f5874ec2204a5c95ef0
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-01 12:53:21 -0600

    Finish relative layout aplication

---
+++ Test1/app/src/main/java/com/example/test1/RelativeLayoutAplicacion.kt
@@ -0,0 +1,11 @@
+package com.example.test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayoutAplicacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout_aplicacion)
+    }
+}
