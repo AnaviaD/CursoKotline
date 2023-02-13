cfb605bfe1645eff1666dd42a15e1c426240774c
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-13 16:25:09 -0600

    update add_task

--- Test1/app/src/main/java/com/example/test1/add_task.kt
+++ Test1/app/src/main/java/com/example/test1/add_task.kt
@@ -2,8 +2,19 @@ package com.example.test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.test1.database.DatabaseHelper
 
 class add_task : AppCompatActivity() {
+
+    lateinit var btn_save: Button
+    lateinit var btn_del: Button
+    lateinit var et_name: EditText
+    lateinit var et_details: EditText
+    var dbHandler: DatabaseHelper? = null
+    var isEditMode: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
