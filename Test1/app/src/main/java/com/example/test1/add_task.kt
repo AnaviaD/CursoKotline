9cd8ea36be7f179c8e33f66f7ccde3b351536a34
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-13 16:50:52 -0600

    add_task

--- Test1/app/src/main/java/com/example/test1/add_task.kt
+++ Test1/app/src/main/java/com/example/test1/add_task.kt
@@ -1,10 +1,15 @@
 package com.example.test1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.test1.database.DatabaseHelper
+import com.example.test1.model.TaskListModel
 
 class add_task : AppCompatActivity() {
 
@@ -18,5 +23,41 @@ class add_task : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_task)
+
+        btn_save = findViewById(R.id.btn_save)
+        btn_del = findViewById(R.id.btn_del)
+        et_name = findViewById(R.id.et_name)
+        et_details = findViewById(R.id.et_details)
+
+        dbHandler = DatabaseHelper(this)
+
+        if(intent != null && intent.getStringExtra("Mode") =="E"){
+            isEditMode = true
+            btn_save.text = "Update Data"
+            btn_del.visibility = View.VISIBLE
+        }else{
+            isEditMode = false
+            btn_save.text = "Save Data"
+            btn_del.visibility = View.GONE
+        }
+        btn_save.setOnClickListener {
+        var success: false
+        var tasks: TaskListModel = TaskListModel()
+            if (isEditMode){
+
+            }else{
+                tasks.name = et_name.text.toString()
+                tasks.details = et_details.text.toString()
+
+                success = dbHandler?.addTask(tasks) as Boolean
+            }
+            if (success){
+                val i = Intent(applicationContext, aMain::class.java)
+                startActivity(i)
+                finish()
+            }else{
+                Toast.makeText(applicationContext, "Something went Wrong!", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
