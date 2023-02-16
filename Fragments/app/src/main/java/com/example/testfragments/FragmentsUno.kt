9c71513547ee9ada8464c58a95a2568f5f38d22b
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-16 16:56:51 -0600

    Finish fragment example

--- Fragments/app/src/main/java/com/example/testfragments/FragmentsUno.kt
+++ Fragments/app/src/main/java/com/example/testfragments/FragmentsUno.kt
@@ -34,7 +34,7 @@ class FragmentsUno : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragments_uno, container, false)
+        return inflater.inflate(R.layout.fragment_uno, container, false)
     }
 
     companion object {
--- Fragments/app/src/main/java/com/example/testfragments/MainActivity.kt
+++ Fragments/app/src/main/java/com/example/testfragments/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.testfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.testfragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val transaccion = supportFragmentManager.beginTransaction()
+            val fragmento = FragmentsUno()
+            transaccion.replace(R.id.contenedorFragments, fragmento)
+            transaccion.addToBackStack(null)
+            transaccion.commit()
+        }
+
+        binding.button2.setOnClickListener{
+            val transaccion2 = supportFragmentManager.beginTransaction()
+            val fragmento2 = FragmentDos()
+            transaccion2.replace(R.id.contenedorFragments, fragmento2)
+            transaccion2.addToBackStack(null)
+            transaccion2.commit()
+        }
+
+        binding.button3.setOnClickListener{
+            val transaccion3 = supportFragmentManager.beginTransaction()
+            val fragmento3 = FragmentTres()
+            transaccion3.replace(R.id.contenedorFragments, fragmento3)
+            transaccion3.addToBackStack(null)
+            transaccion3.commit()
+        }
     }
 }
