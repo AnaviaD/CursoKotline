6ed906bac28d1468ab2aa6c661dbae3fde9f042e
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-02-17 12:43:39 -0600

    Send data from main to fragment

--- FragmentsData/app/src/main/java/com/example/datatrufragments/MainActivity.kt
+++ FragmentsData/app/src/main/java/com/example/datatrufragments/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.datatrufragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.datatrufragments.Fragments.FragmentDos
+import com.example.datatrufragments.Fragments.FragmentTres
+import com.example.datatrufragments.Fragments.FragmentUno
+import com.example.datatrufragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    var numFragmento = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            val bundle = Bundle()
+            bundle.putInt("numero", ++numFragmento)
+            bundle.putInt("numero1", 5)
+            bundle.putInt("numero2", 9)
+
+            val transaccion = supportFragmentManager.beginTransaction()
+            val fragmento = FragmentUno()
+
+            fragmento.arguments = bundle
+
+            transaccion.replace(R.id.contenedor, fragmento)
+            transaccion.addToBackStack(null)
+            transaccion.commit()
+        }
+
+        binding.button2.setOnClickListener {
+            val transaccion = supportFragmentManager.beginTransaction()
+            val fragmento = FragmentDos()
+
+            transaccion.replace(R.id.contenedor, fragmento)
+            transaccion.addToBackStack(null)
+            transaccion.commit()
+        }
+
+        binding.button3.setOnClickListener {
+            val transaccion = supportFragmentManager.beginTransaction()
+            val fragmento = FragmentTres()
+
+            transaccion.replace(R.id.contenedor, fragmento)
+            transaccion.addToBackStack(null)
+            transaccion.commit()
+        }
     }
 }
