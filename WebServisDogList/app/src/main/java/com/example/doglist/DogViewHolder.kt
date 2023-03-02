04f180844df10056daa48b4b16d96233b300254b
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-02 13:18:44 -0600

    Finish the DogAdapter

--- WebServisDogList/app/src/main/java/com/example/doglist/DogViewHolder.kt
+++ WebServisDogList/app/src/main/java/com/example/doglist/DogViewHolder.kt
@@ -2,10 +2,15 @@ package com.example.doglist
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.example.doglist.databinding.ItemDogBinding
+import com.squareup.picasso.Picasso
 
 
 class DogViewHolder(view:View):RecyclerView.ViewHolder(view){
-    fun bind(image: String){
 
+    private val binding = ItemDogBinding.bind(view)
+
+    fun bind(image: String){
+        Picasso.get().load(image).into(binding.ivDog)
     }
 }
