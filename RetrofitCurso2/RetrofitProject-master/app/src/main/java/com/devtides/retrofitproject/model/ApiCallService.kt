26afd67e74ab10327151df8b3d399392fd96218f
Alberto Navia <105769948+AnaviaD@users.noreply.github.com>
2023-03-17 12:49:48 -0600

    Add the final / to fix error

--- RetrofitCurso2/RetrofitProject-master/app/src/main/java/com/devtides/retrofitproject/model/ApiCallService.kt
+++ RetrofitCurso2/RetrofitProject-master/app/src/main/java/com/devtides/retrofitproject/model/ApiCallService.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiCallService {
 
-    private val BASE_URL = "http://10.1.1.244:82/api/Intercambio/GetIntercambioUnidad"
+    private val BASE_URL = "http://10.1.1.244:82/api/Intercambio/GetIntercambioUnidad/"
 
     private val api = Retrofit.Builder()
         .baseUrl(BASE_URL)
