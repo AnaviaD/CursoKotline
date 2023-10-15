cd017cc553b901a1f2d578f0f98e9037e97c4958
Enrique Alberto Navia <anavia_n@outlook.com>
2023-10-15 12:43:54 -0600

    Jobs, isActive y closeJobs

--- Corrutina/app/src/main/java/com/example/corrutina/Clases/Clase5.kt
+++ Corrutina/app/src/main/java/com/example/corrutina/Clases/Clase5.kt
@@ -2,11 +2,21 @@ package com.example.corrutina.Clases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.corrutina.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class Clase5 : AppCompatActivity() {
+
+    val TAG = "Clase5"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clase5)
+
     }
 }
