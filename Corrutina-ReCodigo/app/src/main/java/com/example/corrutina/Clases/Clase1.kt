a63f54097e152916211acc638aa9bd6e85814aed
Enrique Alberto Navia <anavia_n@outlook.com>
2023-11-20 17:40:13 -0600

    Corrutinas1

--- Corrutina-ReCodigo/app/src/main/java/com/example/corrutina/Clases/Clase1.kt
+++ Corrutina-ReCodigo/app/src/main/java/com/example/corrutina/Clases/Clase1.kt
@@ -2,11 +2,16 @@ package com.example.corrutina.Clases
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.os.IResultReceiver.Default
 import android.util.Log
 import com.example.corrutina.R
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.time.Duration
+import kotlin.time.measureTime
 
 class Clase1 : AppCompatActivity() {
 
@@ -15,6 +20,19 @@ class Clase1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clase1)
 
+        runFirst()
+
+    }
+
+    fun runFirst(){
+        GlobalScope.launch {
+            val time: kotlin.time.Duration = measureTime {
+                Log.d(TAG, "print this NOW")
+                delay(1000L)
+                Log.d(TAG, "print this after 1 second ")
+            }
+            Log.d(TAG, "duration: ${time.inWholeMilliseconds}")
+        }
     }
 
 }
