682e9c775136bda1c1be5927b0270f32b0b1f9f7
Enrique Alberto Navia <anavia_n@outlook.com>
2023-12-24 14:03:16 -0600

    Nulable

--- Curso12HrsDir/Curso12Hrs/app/src/main/java/com/drack/curso12hrs/When.kt
+++ Curso12HrsDir/Curso12Hrs/app/src/main/java/com/drack/curso12hrs/When.kt
@@ -7,27 +7,31 @@ fun main()
 
     getMonth(2)
     getTrimestre(3)
-    result(arg)
+    result(5)
 
 }
 
 fun result(value    :Any){
     when (value){
-        is  Int     ->  getTrimestre(value)
+        is  Int     ->  println( getTrimestre(value))
         is String   ->  println("Es string")
         is Boolean  ->  if (value) println("El valor es Bool") else println("Adios perdedor")
     }
 }
 
-fun getTrimestre(trimestre: Int) {
-    when(trimestre){
-        1, 2, 3             ->  println("Primer trimestre")
-        4, 5, 6             ->  println("Segundo trimestre")
-        7, 8, 9             ->  println("Tercer trimestre")
-        10, 11, 12          ->  println("Cuarto trimestre")
-        in 13 ..100   ->  println("Primer rango")
+fun getTrimestre(trimestre: Int)    :String {
+    return when (trimestre) {
+        1, 2, 3 -> "Primer trimestre"
+        4, 5, 6 -> "Segundo trimestre"
+        7, 8, 9 -> "Tercer trimestre"
+        10, 11, 12 -> "Cuarto trimestre"
+        in 13..100 -> "Primer rango"
+        else -> {
+            "Eso no existe"
+        }
     }
 }
+
 
 fun getMonth(month  :Int){
     when(month){
